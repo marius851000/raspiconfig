e82a44cd255244b420ed8137f3003841e27632aa
marius david <mariusdavid@laposte.net>
2021-11-23 22:03:27 +0100

    changed the bootloader to prevent the system not booting

--- hardware.nix
+++ hardware.nix
@@ -2,22 +2,26 @@
 
 {
     boot.loader.grub.enable = false;
-    boot.kernelParams = ["cma=128M"];
-    boot.loader.raspberryPi.enable = true;
-    boot.loader.raspberryPi.version = 3;
-    boot.loader.raspberryPi.uboot.enable = true;
-    boot.loader.raspberryPi.firmwareConfig = ''
-        gpu_mem=128
-    ''; #switch to 256 if xserver is needed.
-
+    boot.loader.generic-extlinux-compatible.enable = true;
+    boot.consoleLogLevel = 7;
     boot.kernelPackages = pkgs.linuxPackages_latest;
+
+#    boot.kernelParams = ["cma=128M"];
+#    boot.loader.raspberryPi.enable = true;
+#    boot.loader.raspberryPi.version = 3;
+#    boot.loader.raspberryPi.uboot.enable = true;
+#    boot.loader.raspberryPi.firmwareConfig = ''
+#        gpu_mem=128
+#    ''; #switch to 256 if xserver is needed.
+
+#    boot.kernelPackages = pkgs.linuxPackages_latest;
     
-    hardware.deviceTree.filter = "*rpi*.dtb";
+#    hardware.deviceTree.filter = "*rpi*.dtb";
     fileSystems = {
-    "/boot" = {
-      device = "/dev/disk/by-label/NIXOS_BOOT";
-      fsType = "vfat";
-    };
+#    "/boot" = {
+#      device = "/dev/disk/by-label/NIXOS_BOOT";
+#      fsType = "vfat";
+#    };
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
