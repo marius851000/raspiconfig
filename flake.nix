250212d840dab064573d6b1a5c79d8fe6919302f
marius david <mariusdavid@laposte.net>
2021-11-23 22:02:53 +0100

    do not compile the site locally for performance reason

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
     inputs.nixpkgs.url = "nixpkgs";
-    inputs.pmdsite = {
-        url = "github:marius851000/pmd_hack_weekly";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
+#    inputs.pmdsite = {
+#        url = "github:marius851000/pmd_hack_weekly";
+#        inputs.nixpkgs.follows = "nixpkgs";
+#    };
 
-    outputs = { self, nixpkgs, pmdsite }: {
+    outputs = { self, nixpkgs }: {
         nixosConfigurations.marius-rasberrypi = nixpkgs.lib.nixosSystem {
             system = "aarch64-linux";
             modules = [
