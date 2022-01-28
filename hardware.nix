83eea0f7f7e9e81d9544fc8c287c7aa8141df648
marius david <mariusdavid@laposte.net>
2022-01-28 20:56:24 +0100

    changed io scheduler

--- hardware.nix
+++ hardware.nix
@@ -1,27 +1,29 @@
 { config, pkgs, ... }:
 
 {
-    boot.loader.grub.enable = false;
-    boot.loader.generic-extlinux-compatible.enable = true;
-    boot.consoleLogLevel = 7;
-    boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+  boot.consoleLogLevel = 7;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
-#    boot.kernelParams = ["cma=128M"];
-#    boot.loader.raspberryPi.enable = true;
-#    boot.loader.raspberryPi.version = 3;
-#    boot.loader.raspberryPi.uboot.enable = true;
-#    boot.loader.raspberryPi.firmwareConfig = ''
-#        gpu_mem=128
-#    ''; #switch to 256 if xserver is needed.
+  boot.kernelParams = [ "elevator=bfq" ];
 
-#    boot.kernelPackages = pkgs.linuxPackages_latest;
-    
-#    hardware.deviceTree.filter = "*rpi*.dtb";
-    fileSystems = {
-#    "/boot" = {
-#      device = "/dev/disk/by-label/NIXOS_BOOT";
-#      fsType = "vfat";
-#    };
+  #    boot.kernelParams = ["cma=128M"];
+  #    boot.loader.raspberryPi.enable = true;
+  #    boot.loader.raspberryPi.version = 3;
+  #    boot.loader.raspberryPi.uboot.enable = true;
+  #    boot.loader.raspberryPi.firmwareConfig = ''
+  #        gpu_mem=128
+  #    ''; #switch to 256 if xserver is needed.
+
+  #    boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  #    hardware.deviceTree.filter = "*rpi*.dtb";
+  fileSystems = {
+    #    "/boot" = {
+    #      device = "/dev/disk/by-label/NIXOS_BOOT";
+    #      fsType = "vfat";
+    #    };
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
