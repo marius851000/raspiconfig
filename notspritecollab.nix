05402ed8093a4add0d2e6e76a8be7056433ea6a4
marius david <mariusdavid@laposte.net>
2022-03-23 18:31:43 +0100

    define a user for notspritecollab

--- notspritecollab.nix
+++ notspritecollab.nix
@@ -2,10 +2,13 @@
 { pkgs, ... }:
 
 let
+    storagePath = "/var/lib/notspritecollab";
     spritebot = pkgs.callPackage ./spritebot.nix {
-        storagePath = "/notspritecollab";
+        inherit storagePath;
         src = spritebot_src;
     };
+    user = "Eaglace";
+    group = "Eaglace";
 in
 {
     systemd.services.notspritecollabbot = {
@@ -17,6 +20,23 @@ in
             ExecStart = "${spritebot}/bin/spritebot";
             Restart = "on-failure";
             RestartSec = 65;
+            User = user;
+            Group = group;
         };
     };
+
+    systemd.tmpfiles.rules = [
+        "d '${storagePath}' 700 ${user} ${group} -"
+        "d '${storagePath}.private' 700 ${user} ${group} -"
+    ];
+
+    users.users."${user}" = {
+        description = "NotSpriteBot user";
+        group = group;
+        isSystemUser = true;
+    };
+
+    users.groups."${group}" = {};
+
+    #TODO: put this in his own flake -- maybe upstream, in the spritebot repo
 }
