af162a892c7587c7338cf21c1d5d162e00114741
marius david <mariusdavid@laposte.net>
2022-03-23 22:10:27 +0100

    never got my fr.eu.org domain...

--- mariusdavid.fr.nix
+++ mariusdavid.fr.nix
@@ -2,12 +2,14 @@
 
 {
   SOA = {
-    nameServer = "marius851000.fr.eu.org.";
+    nameServer = "mariusdavid.fr.";
     adminEmail = "mariusdavid@laposte.net";
     serial = 10001;
   };
 
   NS = [ "marius851000.fr.eu.org." ];
+
+  TXT = [ "test" ];
 
   A = [ "51.38.185.177" ];
   AAAA = [ "2001:41d0:0305:2100:0000:0000:0000:9331" ];
