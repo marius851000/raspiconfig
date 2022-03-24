e7594fe8f6b9129a1eb5db494b5296dafe96bab9
marius david <mariusdavid@laposte.net>
2022-03-24 19:59:13 +0100

    github archive secured !

--- python-github-archive.nix
+++ python-github-archive.nix
@@ -7,6 +7,12 @@ let
   python-github-archive = mach-nix.lib."${system}".buildPythonApplication {
     src = python-github-archive_src;
   };
+
+  tokenPath = "/secret-github-bot-token.txt";
+  backupDir = "/githubback";
+
+  user = "githubback";
+  group = "githubback";
 in
 {
   environment.systemPackages = [ python-github-archive ];
@@ -22,21 +28,47 @@ in
         wantedBy = [ "timers.target" ];
       };
     };
-    #TODO: average systemd hardening
+
     services = {
       backup_github = {
         description = "backup github data";
         path = [ pkgs.git ];
 
+        environment = {
+            GIT_SSL_CAINFO = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+            SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+        };
+
+        confinement.enable = true;
+        confinement.packages = [ pkgs.cacert pkgs.git ];
+
         serviceConfig = {
           Type = "oneshot";
           ExecStart = pkgs.writeScript "backup-github.sh" ''
             #!${pkgs.bash}/bin/bash
-            export GITHUB_TOKEN=$(cat /secret-github-bot-token.txt)
-            ${python-github-archive}/bin/github-backup SkyTemple --organization --output-directory /githubback --token $GITHUB_TOKEN -i --repositories --fork
+            export GITHUB_TOKEN=$(${pkgs.coreutils}/bin/cat ${tokenPath})
+            ${python-github-archive}/bin/github-backup SkyTemple --organization --output-directory ${backupDir} --token $GITHUB_TOKEN -i --repositories --fork
           '';
+          User = user;
+          Group = group;
+
+          BindPaths="${backupDir}";
+          BindReadOnlyPaths="${tokenPath} /etc";
         };
       };
     };
   };
+
+  systemd.tmpfiles.rules = [
+    "f '${tokenPath}' 400 ${user} ${group} -"
+    "d '${backupDir}' 700 ${user} ${group} -"
+  ];
+
+  users.users."${user}" = {
+    description = "NotSpriteBot user";
+    group = group;
+    isSystemUser = true;
+  };
+
+  users.groups."${group}" = { };
 }
