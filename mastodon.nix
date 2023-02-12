979f4d8bd45aed8bb6a777cf7637bf49aff19b07
marius david <marius@mariusdavid.fr>
2023-02-12 09:32:34 +0100

    use integrated mastodon GC now

--- mastodon.nix
+++ mastodon.nix
@@ -6,7 +6,8 @@
   services.mastodon = {
     enable = true;
     webProcesses = 1;
-    sidekiqThreads = 2;
+    webThreads = 2;
+    sidekiqThreads = 1;
     localDomain = "mariusdavid.fr";
     smtp = {
       authenticate = true;
@@ -63,20 +64,6 @@
         proxyPass ="http://unix:/run/mastodon-streaming/streaming.socket";
         proxyWebsockets = true;
       };
-    };
-  };
-
-  systemd.services.gc_mastodon_cache = {
-    serviceConfig.Type = "oneshot";
-    serviceConfig.User = "mastodon";
-    serviceConfig.ExecStart = "/etc/profiles/per-user/mastodon/bin/mastodon-env /etc/profiles/per-user/mastodon/bin/tootctl media remove --days=0 --concurrency=1";
-  };
-  systemd.timers.gc_mastodon_cache = {
-    wantedBy = [ "postgresql.target" ];
-    partOf = [ "gc_mastodon_cache.service" ];
-    timerConfig = {
-      OnCalendar = "*-*-* *:04:00";
-      Unit = "gc_mastodon_cache.service";
     };
   };
 }
