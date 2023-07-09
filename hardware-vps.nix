4763e4db9d0b08631befa97e06c9837d0970ab3b
marius david <marius@mariusdavid.fr>
2023-07-09 13:53:57 +0200

    update VPS definition

--- hardware-vps.nix
+++ hardware-vps.nix
@@ -3,7 +3,23 @@
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
   boot.loader.grub.device = "/dev/sda";
   boot.initrd.kernelModules = [ "nvme" ];
-  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+  boot.supportedFilesystems = [ "btrfs" "vfat" "ntfs" ];
+  fileSystems."/" = {
+    device = "/dev/sda1";
+    fsType = "btrfs";
+    options = [ "compress=zstd" ];
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/sda15";
+    fsType = "ext2";
+  };
+
+  services.beesd.filesystems.root = {
+    spec = "/";
+    hashTableSizeMB = 1024;
+    verbosity = 7;
+  };
 
   #was: vps-b4203bd5
   networking.hostName = "otulissa";
@@ -21,8 +37,14 @@
     };
   };
 
+  networking = {
+    domain = "hacknews.pmdcollab.org";
+  };
+
   networking.defaultGateway6 = {
     address = "2001:41d0:305:2100::1";
     interface = "ens3";
   };
+
+  swapDevices = [ { device = "/swapfile"; size = 2048; } ];
 }
