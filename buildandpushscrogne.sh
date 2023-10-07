78dd5400c939b506cb85744792150d544c74baf7
marius david <marius@mariusdavid.fr>
2023-10-07 15:34:08 +0200

    some file that will stay less long on here.

--- buildandpushscrogne.sh
+++ buildandpushscrogne.sh
@@ -1,2 +1,2 @@
-nixos-rebuild switch --target-host root@5.196.70.120 --flake .#scrogne --keep-going -j 7 --impure #--show-trace -vv #--option substituters "" #--show-trace
+nixos-rebuild switch --target-host root@mariusdavid.fr --flake .#scrogne --keep-going -j 7 --impure #--show-trace -vv #--option substituters "" #--show-trace
 #--keep-going #--build-host raspi
