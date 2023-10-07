247a14115d24d85192baeb7dc73022a836e421f3
marius david <marius@mariusdavid.fr>
2023-10-07 15:30:14 +0200

    fix lemmy

--- lemmy.nix
+++ lemmy.nix
@@ -3,16 +3,17 @@
 {
   security.acme.certs."mariusdavid.fr".extraDomainNames = [ "lemmy.mariusdavid.fr" ];
 
+  #services.postgresql.package = pkgs.postgresql_15;
+
   services.lemmy = {
     enable = true;
     nginx.enable = true;
     settings = {
       hostname = "lemmy.mariusdavid.fr";
-      database.host = "localhost";
-      database.password = "localdbpass";
       #database.port = null;
     };
     database.createLocally = true;
+    database.uri = "postgres:///lemmy?host=/run/postgresql&user=lemmy";
   };
 
   services.nginx.virtualHosts."lemmy.mariusdavid.fr" = {
@@ -20,7 +21,7 @@
     forceSSL = true;
   };
   
-  systemd.services.lemmy.environment.LEMMY_DATABASE_URL = pkgs.lib.mkForce "postgres:///lemmy?host=/run/postgresql&user=lemmy";
+  #systemd.services.lemmy.environment.LEMMY_DATABASE_URL = pkgs.lib.mkForce "postgres:///lemmy?host=/run/postgresql&user=lemmy";
 
   services.pict-rs.port = 5934;
 }
