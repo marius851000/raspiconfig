13eefb2e6c9b820a4fa3aa102ef2956bdcbea878
marius david <marius@mariusdavid.fr>
2023-10-08 13:21:23 +0200

    removed few old stuff

--- buildandpush.sh
+++
@@ -1,2 +0,0 @@
-nixos-rebuild switch --target-host root@51.38.185.177 --flake .#otulissa --keep-going -j 7 --impure #--show-trace -vv #--option substituters "" #--show-trace
-#--keep-going #--build-host raspi
