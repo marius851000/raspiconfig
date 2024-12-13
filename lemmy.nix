4a6db60f3fcb5b0ff91ad76bf582c5d3d2ad0f0a
marius david <marius@mariusdavid.fr>
2024-12-13 19:57:16 +0100

    better lemmy

--- lemmy.nix
+++ lemmy.nix
@@ -1,4 +1,4 @@
-{ pkgs, ...}:
+{ pkgs, config, ...}:
 
 {
   security.acme.certs."mariusdavid.fr".extraDomainNames = [ "lemmy.mariusdavid.fr" ];
@@ -11,7 +11,14 @@
     settings = {
       hostname = "lemmy.mariusdavid.fr";
       #database.port = null;
+      email = {
+        smtp_server = "mariusdavid.fr:587";
+        smtp_login = "grafana@mariusdavid.fr";
+        smtp_from_address = "grafana@mariusdavid.fr";
+        tls_type = "startls";
+      };
     };
+    smtpPasswordFile = "/secret-mail-grafana.txt";
     database.createLocally = true;
     database.uri = "postgres:///lemmy?host=/run/postgresql&user=lemmy";
   };
@@ -24,4 +31,7 @@
   #systemd.services.lemmy.environment.LEMMY_DATABASE_URL = pkgs.lib.mkForce "postgres:///lemmy?host=/run/postgresql&user=lemmy";
 
   services.pict-rs.port = 5934;
+  services.pict-rs.package = pkgs.pict-rs;
+
+  systemd.services.pict-rs.environment.PICTRS__SERVER__ADDRESS = "127.0.0.1:5934";
 }
