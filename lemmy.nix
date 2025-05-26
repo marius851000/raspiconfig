cab9505b0a2d41b2bddccdc2235f72c3f17eb193
marius david <marius@mariusdavid.fr>
2025-05-26 18:26:06 +0200

    more lemmy and pict-rs tracing

--- lemmy.nix
+++ lemmy.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ...}:
+{ pkgs, ...}:
 
 {
   security.acme.certs."mariusdavid.fr".extraDomainNames = [ "lemmy.mariusdavid.fr" ];
@@ -17,11 +17,27 @@
         smtp_from_address = "grafana@mariusdavid.fr";
         tls_type = "startls";
       };
+      prometheus = {
+        bind = "127.0.0.1";
+        port = 10002;
+      };
     };
     smtpPasswordFile = "/secret-mail-grafana.txt";
     database.createLocally = true;
     database.uri = "postgres:///lemmy?host=/run/postgresql&user=lemmy";
   };
+
+  services.prometheus.scrapeConfigs = [
+    {
+      job_name = "lemmy";
+      scrape_interval = "20s";
+      static_configs = [
+        {
+          targets = [ "localhost:10002" ];
+        }
+      ];
+    }
+  ];
 
   services.nginx.virtualHosts."lemmy.mariusdavid.fr" = {
     useACMEHost = "mariusdavid.fr";
@@ -34,4 +50,5 @@
   services.pict-rs.package = pkgs.pict-rs;
 
   systemd.services.pict-rs.environment.PICTRS__SERVER__ADDRESS = "127.0.0.1:5934";
+  systemd.services.pict-rs.environment.RUST_LOG = "debug,sled=info";
 }
