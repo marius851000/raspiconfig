5307b041050010b24916bdfb470c8ca514f4d15b
Marius DAVID <git@mariusdavid.fr>
2025-07-25 17:21:49 +0200

    Update syncthing.nix

--- syncthing.nix
+++ syncthing.nix
@@ -9,6 +9,7 @@
     user = "dokuwiki_pool"; #TODO: make this it’s own user and rely on group instead
     settings.devices = {
       "mariuspc" = { id = "5DV5CNV-RINTS7A-QRW7NB2-HVESTUX-4PL7NTS-7SCY7Q2-56ZZGTU-H6LOCQV"; };
+      "coryn" = { id = "UIX2SV2-7QWSEVO-7H2ZER2-V5BT4T4-UCNBSYF-CG6GBE6-RWCOVQ7-VF4VJQP"; };
     };
   };
 
