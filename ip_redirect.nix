f625c6b3ff59156be494628f44420f2bedb5bafc
Marius DAVID <git@mariusdavid.fr>
2025-09-19 17:38:17 +0200

    also redirect stuff to coryn

--- ip_redirect.nix
+++ ip_redirect.nix
@@ -4,6 +4,7 @@
 let
   marella_yggdrasil_ip = "202:3679:f712:fd04:e3de:a123:caf4:580d";
   noctus_yggdrasil_ip = "200:6233:ac7:f76c:ef8f:e313:aa1:b882";
+  coryn_yggdrasil_ip = "201:c608:513e:2269:3d8d:b3eb:93c1:f1e7";
   server_ip = "5.196.70.120";
   server_ip_v6 = "2001:41d0:e:378::1";
 
@@ -22,6 +23,10 @@ let
     "noctus.net.mariusdavid.fr"
     "lemmy.mariusdavid.fr"
   ];
+  domains_to_proxy_to_coryn = [
+    "coryn.net.mariusdavid.fr"
+    "openwebui.mariusdavid.fr"
+  ];
 in
 {
   marinfra.yggdrasil.enable = true;
@@ -38,6 +43,7 @@ in
       map $ssl_preread_server_name $server_redirect {
       ${lib.concatLines (builtins.map (domain: "${domain} https_marella_backend;") domains_to_proxy_to_marella)}
       ${lib.concatLines (builtins.map (domain: "${domain} https_noctus_backend;") domains_to_proxy_to_noctus)}
+      ${lib.concatLines (builtins.map (domain: "${domain} https_coryn_backend;") domains_to_proxy_to_coryn)}
         default https_default_backend;
       }
 
@@ -51,6 +57,10 @@ in
 
       upstream https_noctus_backend {
         server [${noctus_yggdrasil_ip}]:443;
+      }
+
+      upstream https_coryn_backend {
+        server [${coryn_yggdrasil_ip}]:443;
       }
 
       server {
@@ -73,6 +83,12 @@ in
           proxyPass = "http://[${noctus_yggdrasil_ip}]:80";
         };
       }) domains_to_proxy_to_noctus)
+      ++
+      (builtins.map (domain: lib.nameValuePair domain {
+        locations."/" = {
+          proxyPass = "http://[${coryn_yggdrasil_ip}]:80";
+        };
+      }) domains_to_proxy_to_coryn)
     );
   };
 }
