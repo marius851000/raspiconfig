2f0cc73a29ec759d32dc3d761e4cf9f39b31915f
Marius DAVID <git@mariusdavid.fr>
2026-01-31 14:15:05 +0100

    various wiki change

--- nesmy.nix
+++ nesmy.nix
@@ -3,6 +3,10 @@
 {
   marinfra.ssl.extraDomain = [ "nesmy.mariusdavid.fr" ];
 
+  networking.extraHosts = ''
+    127.0.0.0 nesmy.mariusdavid.fr
+  '';
+
   services.mediawiki = {
     enable = true;
     database.type = "mysql";
@@ -10,6 +14,11 @@
       ConfirmEdit = null;
       VisualEditor = null;
       Cite = null;
+      ParserFunctions = null;
+      TitleKey = builtins.fetchGit {
+        url = "https://gerrit.wikimedia.org/r/mediawiki/extensions/TitleKey";
+        rev = "a9255da4b05002600195a3efbc8770913bc172ab";
+      };
     };
 
     # create the user session "cache" table
@@ -59,13 +68,22 @@
     name = "Nesmy Wiki";
 
     phpPackage = pkgs.php83.buildEnv {
-      extensions = { enabled, all }: enabled ++ [ all.apcu all.igbinary ];
+      extensions = { enabled, all }: enabled ++ [ all.apcu ];
     };
   };
 
   services.nginx.virtualHosts."nesmy.mariusdavid.fr" = {
     http3 = true;
     quic = true;
+
+    locations = {
+      "/dumps/" = {
+        alias = "/var/lib/mediawiki_nesmy_dump/";
+        extraConfig = ''
+          autoindex on;
+        '';
+      };
+    };
   };
 
   # opcache is loaded by default
@@ -73,4 +91,38 @@
   # main cache? in place
   # sidebar cache?
   # caching proxy?
+
+  systemd.timers.mediawiki_nesmy_publish_dump = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      RandomizedDelaySec = "3600";
+      FixedRandomDelay = "true";
+      Persistent = "true";
+    };
+  };
+
+  systemd.services.mediawiki_nesmy_publish_dump = {
+    script = ''
+      set -euxo pipefail
+      cd /var/lib/mediawiki_nesmy_dump
+
+      /run/current-system/sw/bin/mediawiki-maintenance dumpBackup.php --full > tmp/ongoing_backup.xml
+      mv tmp/ongoing_backup.xml nesmy-wiki-dump-$(date '+%Y-%m-%d-%H:%M:%S').xml
+      echo "✅ backup generated"
+
+      find . -name "nesmy-wiki-dump-*.xml" -mtime +7 -delete
+      echo "✅ old backup deleted (if it didn’t break horribly)"
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "mediawiki";
+      Group = "nginx";
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d '/var/lib/mediawiki_nesmy_dump' 755 mediawiki nginx -"
+    "d '/var/lib/mediawiki_nesmy_dump/tmp' 700 mediawiki nginx -"
+  ];
 }
